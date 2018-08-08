6f5f00a041617d7a0fb45411e6f4e28c77200dbb
Alassane Niang <adniang75@gmail.com>
2018-08-08 10:57:55 +0200

    Developing FirstProjectApp

--- first-project/src/main/java/com/alassaneniang/FirstProjectApplication.java
+++ first-project/src/main/java/com/alassaneniang/FirstProjectApplication.java
@@ -1,10 +1,20 @@
 package com.alassaneniang;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class FirstProjectApplication {
+    
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World from Spring Boot";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstProjectApplication.class, args);
